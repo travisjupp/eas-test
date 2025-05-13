7a09cfda15e5eaf1cb2ae4a531df32badb4a1f81
tjupp <travisjupp@gmail.com>
2025-05-13 12:08:24 -0600

    Configure react-native-paper: extend theme

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,25 @@
-import { Text, View } from "react-native";
-import React, { useState } from "react";
+import * as React from "react";
+import { Text, View, AppRegistry } from "react-native";
+import { MD3LightTheme as DefaultTheme, PaperProvider } from "react-native-paper";
+import data from "../app.json" with { type: 'json' };
+const appName = data.expo.name;
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
+
+console.log();
 
 export default function Index() {
   return (
-    <>
+    <PaperProvider theme={theme}>
+      {/* other components need useTheme hook, import from 'react-native-paper' */} 
+      {/* const theme = useTheme(); */}
       <View
         style={{
           flex: 1,
@@ -12,7 +27,7 @@ export default function Index() {
           alignItems: "center",
         }}
       >
-        <Text>Edit app/index.tsx to edit this screen.</Text>
+        <Text style={{ color: theme.colors.primary }}>Edit app/index.tsx to edit this screen.</Text>
       </View>
       <View style={{
         flex: 1,
@@ -21,6 +36,9 @@ export default function Index() {
       }}>
         <Text style={{fontWeight: 700}}>TEST</Text>
       </View>
-    </>
+    </PaperProvider>
   );
 }
+
+AppRegistry.registerComponent(appName, () => Main);
+
